be4c03a73c01072c8e1c633834075cc45745afb3
Nick <nick.dodig@gmail.com>
2021-06-30 18:40:52 +1000

    Implemented morgan and winston logging.

--- analysis-tool-server/server.js
+++ analysis-tool-server/server.js
@@ -2,42 +2,42 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const morganLogger = require('./config/morgan-logger');
+const winstonLogger = require('./config/winston-logger');
 const router = require('./src/api/routes/index');
-//const db = require('./config/db')
-
-var app = express();
+//db = require('./config/db'),
+app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //app.use(db);
-app.use(logger('common'));
+app.use(morganLogger);
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(router);
 
 // catch 404 and forward to error handler 
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  winstonLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method}`);
+    winstonLogger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method}`);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
